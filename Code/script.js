5892e0c82f2eb415176af39e8b3a77b9844ec9ed
Grimidk <thegrimidk@gmail.com>
2024-02-10 21:03:02 -0400

    javascript

---
+++ Code/script.js
@@ -0,0 +1,43 @@
+function goPlay() { 
+    window.open("play.html"); 
+    window.close(window.self);
+}
+function goStats() { 
+    window.open("stats.html"); 
+    window.close(window.self);
+}
+function goCreds() { 
+    window.open("creds.html"); 
+    window.close(window.self);
+}
+function goIndex() { 
+    window.open("index.html"); 
+    window.close(window.self);
+}
+function goThree() {
+    let user1 = document.getElementById("user1");
+    let user2 = document.getElementById("user1");
+    let user3 = document.getElementById("user1");
+    let user4 = document.getElementById("user1");
+    window.open("playThree.html"); 
+    window.close(window.self);
+}
+function goFour() {
+    let user1 = document.getElementById("user1");
+    let user2 = document.getElementById("user1");
+    let user3 = document.getElementById("user1");
+    let user4 = document.getElementById("user1");
+    window.open("playFour.html"); 
+    window.close(window.self);
+}
+function goFive() {
+    let user1 = document.getElementById("user1");
+    let user2 = document.getElementById("user1");
+    let user3 = document.getElementById("user1");
+    let user4 = document.getElementById("user1");
+    window.open("playFive.html"); 
+    window.close(window.self);
+}
+function names() {  
+    window.alert(user1);
+}

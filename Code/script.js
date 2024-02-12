af5e8da936962ab3db0abb6a3a150f12dab7ab05
Grimidk <thegrimidk@gmail.com>
2024-02-11 23:32:19 -0400

    final

--- Code/script.js
+++ Code/script.js
@@ -29,15 +29,34 @@ function goFive() {
     window.open("playFive.html"); 
     window.close(window.self);
 }
+function defaultNames() {
+    let player1 = document.querySelector('#user1');
+    let player2 = document.querySelector('#user2');
+    let player3 = document.querySelector('#user3');
+    let player4 = document.querySelector('#user4');
+    if(player1.value == "") {
+        player1.value = "Jugador 1"
+    }
+    if(player2.value == "") {
+        player2.value = "Jugador 2"
+    }
+    if(player3.value == "") {
+        player3.value = "Jugador 3"
+    }
+    if(player4.value == "") {
+        player4.value = "Jugador 4"
+    }
+}
 function getNames() {
-    let user1 = document.querySelector('#user1').value;
-    let user2 = document.querySelector('#user2').value;
-    let user3 = document.querySelector('#user3').value;
-    let user4 = document.querySelector('#user4').value;
-    localStorage.setItem("user1", user1)
-    localStorage.setItem("user2", user2)
-    localStorage.setItem("user3", user3)
-    localStorage.setItem("user4", user4)
+    defaultNames()
+    let value1 = document.querySelector('#user1').value;
+    let value2 = document.querySelector('#user2').value;
+    let value3 = document.querySelector('#user3').value;
+    let value4 = document.querySelector('#user4').value;
+    localStorage.setItem("user1", value1)
+    localStorage.setItem("user2", value2)
+    localStorage.setItem("user3", value3)
+    localStorage.setItem("user4", value4)
 }
 function setNames() { 
     player1.textContent = localStorage.getItem("user1")
@@ -54,48 +73,68 @@ function hidePlayers() {
     if (activePlayer.value == "player1"){
         player1.setAttribute("style", "display: grid")
         player1grid.setAttribute("style", "display: grid")
+        player1points.setAttribute("style", "display: grid")
         player2.setAttribute("style", "display: none")
         player2grid.setAttribute("style", "display: none")
+        player2points.setAttribute("style", "display: none")
         player3.setAttribute("style", "display: none")
         player3grid.setAttribute("style", "display: none")
+        player3points.setAttribute("style", "display: none")
         player4.setAttribute("style", "display: none")
         player4grid.setAttribute("style", "display: none")
+        player4points.setAttribute("style", "display: none")
     } else if (activePlayer.value == "player2"){
         player1.setAttribute("style", "display: none")
         player1grid.setAttribute("style", "display: none")
+        player1points.setAttribute("style", "display: none")
         player2.setAttribute("style", "display: grid")
         player2grid.setAttribute("style", "display: grid")
+        player2points.setAttribute("style", "display: grid")
         player3.setAttribute("style", "display: none")
         player3grid.setAttribute("style", "display: none")
+        player3points.setAttribute("style", "display: none")
         player4.setAttribute("style", "display: none")
         player4grid.setAttribute("style", "display: none")
+        player4points.setAttribute("style", "display: none")
     } else if (activePlayer.value == "player3"){
         player1.setAttribute("style", "display: none")
         player1grid.setAttribute("style", "display: none")
+        player1points.setAttribute("style", "display: none")
         player2.setAttribute("style", "display: none")
         player2grid.setAttribute("style", "display: none") 
+        player2points.setAttribute("style", "display: none")
         player3.setAttribute("style", "display: grid")
         player3grid.setAttribute("style", "display: grid")
+        player3points.setAttribute("style", "display: grid")
         player4.setAttribute("style", "display: none")
         player4grid.setAttribute("style", "display: none")
+        player4points.setAttribute("style", "display: none")
     } else if (activePlayer.value == "player4"){
         player1.setAttribute("style", "display: none")
         player1grid.setAttribute("style", "display: none")
+        player1points.setAttribute("style", "display: none")
         player2.setAttribute("style", "display: none")
         player2grid.setAttribute("style", "display: none")
+        player2points.setAttribute("style", "display: none")
         player3.setAttribute("style", "display: none")
         player3grid.setAttribute("style", "display: none")
+        player3points.setAttribute("style", "display: none")
         player4.setAttribute("style", "display: grid")
         player4grid.setAttribute("style", "display: grid")
+        player4points.setAttribute("style", "display: grid")
     } else {
         player1.setAttribute("style", "display: grid")
         player1grid.setAttribute("style", "display: grid")
+        player1points.setAttribute("style", "display: grid")
         player2.setAttribute("style", "display: grid")
         player2grid.setAttribute("style", "display: grid")
+        player2points.setAttribute("style", "display: grid")
         player3.setAttribute("style", "display: grid")
         player3grid.setAttribute("style", "display: grid")
+        player3points.setAttribute("style", "display: grid")
         player4.setAttribute("style", "display: grid")
         player4grid.setAttribute("style", "display: grid")
+        player4points.setAttribute("style", "display: grid")
     }
 }
 function genNums() {
@@ -108,16 +147,27 @@ function fillBoxes() {
     }
 }
 let turn = 0
-function nextTurn() {
+function nextTurn(n) {
     const numElement = document.querySelector('#ultNum');
-    let rando = genNums()
-    numElement.textContent = rando
-    checkBox(rando)
-    //checkScore()
+    let rando = genNums();
+    numElement.textContent = rando;
+    checkBox(rando);
+    let points1 = checkScore(n,1,rando);
+    const player1points = document.querySelector('#player1points');
+    player1points.textContent = points1;
+    // points2 = checkScore(n,2)
+    // const player2points = document.querySelector('#player2points');
+    // player2points.textContent = points2;
+    // points3 = checkScore(n,3)
+    // const player3points = document.querySelector('#player3points');
+    // player3points.textContent = points3;
+    // points4 = checkScore(n,4)
+    // const player4points = document.querySelector('#player4points');
+    // player4points.textContent = points4;
     const turnElement = document.querySelector('#turnCount');
-    let turn = turnElement.textContent
-    turn ++
-    turnElement.textContent = turn
+    let turn = turnElement.textContent;
+    turn ++;
+    turnElement.textContent = turn;
     if(turn >= 25){
         window.alert("Juego terminado!")
         document.querySelector('#btnTurn').disabled = true;
@@ -128,19 +178,21 @@ function checkBox(num) {
     for (let i = 0; i < boxes.length; i++) {
         let box = boxes[i]
         if(box.textContent == num){
-            box.setAttribute("style", "background-color: green")
+            box.setAttribute("style", "background-color: orange")
         }
     }
 }
-function checkScore(){
-    const boxes = document.querySelectorAll(".box");
-    for (let i = 0; i < boxes.length; i++) {
-        let box = boxes[i]
-        if(box.getAttribute("style", "background-color") == green){
-            box.setAttribute("style", "background-color: green")
+function checkScore(n,player,num){
+    playerGrid = "#player"+ player + "gridItem";
+    const items = document.querySelectorAll(playerGrid);
+        for (let i = 0; i < items.length; i++) {
+            let box = items[i]
+            if(box.value == num){
+            points++
         }
+        return points
     }
 }
-function endGame(){
-
-}
+// function endGame(){
+    
+// }
